56522214c15997faf2944879679332733e25369f
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2020-04-04 20:35:49 -0300

    Dados principais carregados no HTML

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ axios
         console.log(response);
 
         var promos = response.data
-        
+
         createCards(promos)
         insertDataInCards(promos)
     })
@@ -25,11 +25,18 @@ function createCards(promos) {
 
 function insertDataInCards(promos) {
     var cardTitle = document.querySelectorAll(".card__title")
+    var cardCover = document.querySelectorAll(".card__cover")
+    var cardDesc = document.querySelectorAll(".card__desc")
+    var cardBtnTerms = document.querySelectorAll("#btn-terms")
+    var cardBtnJoin = document.querySelectorAll("#btn-join")
     var count = 0
 
     for (item of promos) {
-        var dataTitle = item.name
-        cardTitle[count].textContent = dataTitle
+        cardTitle[count].textContent = item.name
+        cardCover[count].setAttribute("src", item.heroImageUrl)
+        cardDesc[count].textContent = item.description
+        cardBtnTerms[count].textContent = item.termsAndConditionsButtonText
+        cardBtnJoin[count].textContent = item.joinNowButtonText
         count++
     }
 }
