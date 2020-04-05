11e68cc44aafac0159f8ebf86ce14cfa36bd305a
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2020-04-05 00:13:55 -0300

    Switch Promos basic function

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ function createCards(promos) {
 }
 
 function insertDataInCards(promos) {
+    var card = document.querySelectorAll(".card")
     var cardTitle = document.querySelectorAll(".card__title")
     var cardCover = document.querySelectorAll(".card__cover")
     var cardDesc = document.querySelectorAll(".card__desc")
@@ -32,11 +33,50 @@ function insertDataInCards(promos) {
     var count = 0
 
     for (item of promos) {
+        if (item.onlyNewCustomers) {
+            card[count].classList.add("onlyNewCustomers")
+        }
+
         cardTitle[count].textContent = item.name
         cardCover[count].setAttribute("src", item.heroImageUrl)
         cardDesc[count].textContent = item.description
         cardBtnTerms[count].textContent = item.termsAndConditionsButtonText
         cardBtnJoin[count].textContent = item.joinNowButtonText
+
         count++
     }
 }
+
+var nav = document.querySelector("nav");
+var btns = nav.querySelectorAll(".nav__button");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.querySelectorAll(".nav__button--active");
+        current[0].className = current[0].className.replace(" nav__button--active", "");
+        this.className += " nav__button--active";
+        if (this.id !== "newCustomers") {
+            var cardNewCustomers = document.querySelectorAll(".onlyNewCustomers")
+            for (x = 0; x < cardNewCustomers.length; x++) {
+                cardNewCustomers[x].style.display = "block"
+            }
+        } else {
+            var cardNewCustomers = document.querySelectorAll(".onlyNewCustomers")
+            for (x = 0; x < cardNewCustomers.length; x++) {
+                cardNewCustomers[x].style.display = "none"
+            }
+        }
+    });
+}
+
+// var btnAllPromos = document.querySelector("#allPromotions")
+// var btnNewCustoms = document.querySelector("#newCustomers")
+
+// btnAllPromos.addEventListener("click", function() {
+//     btnNewCustoms.classList.toggle("nav__button--active")
+//     btnAllPromos.classList.toggle("nav__button--active")
+// })
+
+// btnNewCustoms.addEventListener("click", function() {
+//     btnNewCustoms.classList.toggle("nav__button--active")
+//     btnAllPromos.classList.toggle("nav__button--active")
+// })
